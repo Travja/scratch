75c0b701df82960efbcce4494a0f624b7e7a29da
Travja <the.only.t.craft@gmail.com>
2023-07-29 19:41:14 -0600

    Add origami patterns

---
+++ src/lib/Pattern.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+  export let label: string;
+  export let img: string;
+</script>
+
+<h2>{label}</h2>
+
+<img src={img} alt='{label}'/>
+
+<style>
+  h2, img {
+    display: block;
+    max-width: 80%;
+    margin-inline: auto;
+  }
+
+  img {
+    margin-bottom: 2rem;
+  }
+</style>
--- src/routes/(main)/origami/+page.svelte
+++ src/routes/(main)/origami/+page.svelte
@@ -1,9 +1,29 @@
-<content>
-  <div>There are no patterns yet.</div>
-</content>
+<script>
+  import Pattern from '$lib/Pattern.svelte';
+</script>
+
+<div>
+  <h1>Help us decorate!</h1>
+  <p>Pick a pattern! Fold! Hang it on the tree!</p>
+  <hr />
+  <Pattern img='/patterns/crane.png' label='Crane' />
+  <hr />
+  <Pattern img='/patterns/butterfly.avif' label='Butterfly' />
+  <hr />
+  <Pattern img='/patterns/rabbit.jpg' label='Rabbit' />
+  <hr />
+  <Pattern img='/patterns/swan.jpg' label='Swan' />
+  <hr />
+  <Pattern img='/patterns/heart.jpg' label='Heart' />
+</div>
 
 <style>
-  content {
+  div {
     margin: 1rem;
+  }
+
+  h1, p {
+    display: block;
+    text-align: center;
   }
 </style>
