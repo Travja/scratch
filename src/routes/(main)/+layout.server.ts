e776bd50576809ca5178e551e9c82ffa9aed0836
Travja <the.only.t.craft@gmail.com>
2023-07-30 22:22:13 -0600

    Remove env

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -1,10 +1,7 @@
-import { env } from '$env/dynamic/private';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export const load = async (event: RequestEvent) => {
   const userAgent = event.request.headers.get('user-agent');
-
-  console.log(env.MAX_BODY_SIZE);
 
   return {
     isSafari: userAgent && /(iPhone|iPad)/.test(userAgent)
