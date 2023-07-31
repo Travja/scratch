91a8b001456a9c7895a1a5f67a9ac0deef08c04c
Travja <the.only.t.craft@gmail.com>
2023-07-30 21:40:10 -0600

    Add body size

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -1,7 +1,10 @@
+import { env } from '$env/dynamic/private';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export const load = async (event: RequestEvent) => {
   const userAgent = event.request.headers.get('user-agent');
+
+  console.log(env.MAX_BODY_SIZE);
 
   return {
     isSafari: userAgent && /(iPhone|iPad)/.test(userAgent)
