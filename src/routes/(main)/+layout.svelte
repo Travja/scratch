c4c0782318c4546cd32480f80a8738fdbe0cd698
Travja <the.only.t.craft@gmail.com>
2025-04-18 21:28:57 -0600

    change url

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -36,8 +36,8 @@
     <meta content="StellarMelodies - {pageName}" property="og:title" />
   {/if}
   <meta content="Dorothy and Travis are getting married. Join us for the celebration!" name="og:description" />
-  <meta content="https://stellarmelodies.com" property="og:url" />
-  <meta content="https://stellarmelodies.com/embed-image.jpg" property="og:image" />
+  <meta content="https://stellar.travja.dev" property="og:url" />
+  <meta content="https://stellar.travja.dev/embed-image.jpg" property="og:image" />
   <meta content="#F19985" data-react-helmet="true" name="theme-color" />
 </svelte:head>
 
